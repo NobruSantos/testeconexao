3408f655abd630fc2d451e9768b5fce9a0f2d981
Bruno <brunos422@gmail.com>
2024-02-23 10:18:52 -0300

    Adicionado o arquivo userrs.php

---
+++ users.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="crud">
+        <h3>Lista de usuários da tabela testeconexao</h3>
+        <?php
+        require('../DataBase.php');
+        $DataBase = new DataBase();
+        $sql = "SELECT * FROM testetabela WHERE id > :id";
+        $binds = ['id' => 'id'];
+        $dados = $DataBase -> select($sql, $binds);
+        if($dados -> rowCount() > 0){
+            foreach($dados as $item){
+                echo"<div class='result'>";
+                echo"Nome: {$item['nome']}<br>";
+                echo"Email: {$item['email']}<br>";
+                echo"Descrição: {$item['descricao']}<br>";
+                echo"</div>";
+            }
+        } else {
+                echo "<div class = 'vazio'>";
+                echo "Não temos usuários cadastrados";
+                echo "</div>";
+            }
+        ?>
+    </div>
+</body>
+</html>
